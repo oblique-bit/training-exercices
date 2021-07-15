502c045e7a5d3b716c622e66cbcb9bedf178330c
Ciril Saner <Ciril.Saner@bit.admin.ch>
2021-07-15 15:03:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Training exercices
+
+This project is used for the exercices of the Oblique training.
+
+## Disclaimer
+This project is designed exclusively for training purposes and should not be considered as a basis for a productive project.
+
+## Inquire
+If you would like to schedule a training session or have any other questions, please do not hesitate to contact us by
+email: [Oblique@bit.admin.ch](mailto:Oblique@bit.admin.ch)
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import {NgModule, LOCALE_ID} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {
+	ObMasterLayoutModule,
+	multiTranslateLoader,
+	OB_BANNER,
+	ObExternalLinkModule
+} from '@oblique/oblique';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
+import localeDECH from '@angular/common/locales/de-CH';
+import localeFRCH from '@angular/common/locales/fr-CH';
+import localeITCH from '@angular/common/locales/it-CH';
+import {HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
+import {environment} from '../environments/environment';
+import {HomeComponent} from './home/home.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+registerLocaleData(localeDECH);
+registerLocaleData(localeFRCH);
+registerLocaleData(localeITCH);
+
+@NgModule({
+	declarations: [AppComponent, HomeComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ObMasterLayoutModule, // add other Oblique modules as needed
+		BrowserAnimationsModule,
+		HttpClientModule,
+		TranslateModule.forRoot(multiTranslateLoader()),
+		MatButtonModule,
+		MatCardModule,
+		ObExternalLinkModule
+	],
+	providers: [
+		{provide: LOCALE_ID, useValue: 'de-CH'},
+		{provide: OB_BANNER, useValue: environment.banner}
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule {}
